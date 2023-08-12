6937560d645bf2ae879c7a50a51f5853ec3767ae
NajmyW <120731308+NajmyW@users.noreply.github.com>
2023-08-12 19:14:27 +0700

    Create instagram.js

---
+++ instagram.js
@@ -0,0 +1,20 @@
+async function instagramV2(url) {
+  try {
+  const { data } = await axios.post('https://fastdl.app/c/',{
+    url: url,
+    lang_code: 'en',
+    token: ''
+  },{
+    headers:{
+      'Content-Type':"application/x-www-form-urlencoded; charset=UTF-8",
+      'User-Agent': userAgent()
+    }
+  })
+const parts = data.split('href="').slice(1); 
+const res = parts.map(part => part.split('"')[0].replace(/amp;/g, ''));
+return res
+  } catch (e) {
+  return 404
+  }
+}
+// instagram('https://www.instagram.com/p/CZNzN0dgpsD/')
